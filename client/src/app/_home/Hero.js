4b135860e341f210e3c0861ef2fcd9832dc32b45
farhanpavel <farhanpavel3@gmail.com>
2025-03-01 03:07:54 +0600

    Hero section added

---
+++ client/src/app/_home/Hero.js
@@ -0,0 +1,71 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div>
+      <div className="mt-6 font-bangla">
+        <div className="text-center space-y-5">
+          <div className="space-y-4">
+            <h1 className="text-5xl font-bold leading-[3.6rem]">
+              বিশ্ববিদ্যালয় জীবনকে সহজ করুন <br /> এক প্ল্যাটফর্মেই সব সুবিধা
+            </h1>
+            <p>
+              একাধিক অ্যাপে সময় নষ্ট নয়, <br /> এখন ক্লাস রুটিন, বাস শিডিউল ও
+              ইভেন্ট সব একসাথে!
+            </p>
+          </div>
+          <div className="space-x-5">
+            <Button className="rounded-full px-6 bg-yellow-500 text-white hover:bg-yellow-500">
+              সাইন ইন
+            </Button>
+            <Button className="rounded-full px-6 border-yellow-500 bg-white border-2 text-yellow-500 hover:bg-white hover:text-yellow-500">
+              সাইন আপ
+            </Button>
+          </div>
+        </div>
+        <div className="flex justify-evenly p-6">
+          <div className="relative -mt-24 text-center">
+            <h1 className="bg-[#E54981] rounded-full w-4 h-4"></h1>
+            <Image
+              src={"/images/image1.png"}
+              width={170}
+              height={170}
+              alt="hero1"
+            />
+            <h1 className="bg-[#417CD4] rounded-full w-6 h-6"></h1>
+          </div>
+          <div>
+            <Image
+              src={"/images/image2.png"}
+              width={170}
+              height={170}
+              alt="hero2"
+            />
+            <h1 className="bg-[#FFBA00] rounded-full w-8 h-8 "></h1>
+          </div>
+          <div>
+            <h1 className="bg-[#00C3A5] rounded-full w-6 h-6"></h1>
+            <Image
+              src={"/images/image3.png"}
+              width={170}
+              height={170}
+              alt="hero3"
+            />
+          </div>
+          <div className="relative -mt-24 text-center">
+            <h1 className="bg-purple-600 rounded-full w-6 h-6 absolute left-0 transform -translate-y-1/2"></h1>
+            <Image
+              src={"/images/image4.png"}
+              width={170}
+              height={170}
+              alt="hero4"
+            />
+            <h1 className="bg-[#DD3F3F] rounded-full w-6 h-6 absolute right-0  transform -translate-y-1/2"></h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/components/Header/page.js
+++ client/src/components/Header/page.js
@@ -1,13 +1,143 @@
-import React from "react";
-import { Button } from "../ui/button";
-
+"use client";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuLink,
+} from "@/components/ui/navigation-menu";
+import Image from "next/image";
+import { MenuIcon } from "lucide-react";
 export default function Header() {
   return (
-    <div>
-      <h1 cl>Header</h1>
-      <div className="p-5">
-        <Button className="bg-red-700">Hello</Button>
+    <header className="container mx-auto flex h-20 w-full shrink-0 justify-between items-center px-4 md:px-6 font-bangla ">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon" className="lg:hidden ">
+            <MenuIcon className="h-6 w-6 " />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="bg-white">
+          <Link href="#" prefetch={false}>
+            <Image
+              src="/images/logo.png"
+              width={260}
+              height={200}
+              alt="logo"
+              className="2xl:w-[400px]"
+            />
+            <span className="sr-only">Company Logo</span>
+          </Link>
+          <div className="grid gap-2 py-6 uppercase">
+            <Link
+              href="#benefits"
+              className="flex w-full items-center py-2 text-lg font-bold text-[#212833] "
+              prefetch={false}
+            >
+              সুবিধা
+            </Link>
+            <Link
+              href="#feature"
+              className="flex w-full items-center py-2 text-lg font-bold text-[#212833]"
+              prefetch={false}
+            >
+              বৈশিষ্ট্য
+            </Link>
+            <Link
+              href="#work"
+              className="flex w-full items-center py-2 text-lg font-bold text-[#212833]"
+              prefetch={false}
+            >
+              ব্লগ
+            </Link>
+            <Link
+              href="#work"
+              className="flex w-full items-center py-2 text-lg font-bold text-[#212833]"
+              prefetch={false}
+            >
+              যোগাযোগ
+            </Link>
+          </div>
+        </SheetContent>
+      </Sheet>
+      <div>
+        <Link href="#" className="mr-6 hidden lg:flex  mt-1" prefetch={false}>
+          <Image src="/images/logo.png" width={100} height={100} alt="logo" />
+          <span className="sr-only">Company Logo</span>
+        </Link>
       </div>
-    </div>
+      <div className="uppercase">
+        <NavigationMenu className="hidden lg:flex">
+          <NavigationMenuList className="space-x-14  2xl:space-x-24 mt-2 flex justify-center items-center">
+            <NavigationMenuLink asChild>
+              <Link
+                href="#benefits"
+                className="group inline-flex h-9 w-max items-center justify-center   
+                px-4 py-2 text-lg 2xl:text-2xl font-medium transition-all 
+                text-[#212833] hover:shadow-[0_4px_2px_-2px_rgba(229,73,129,0.9)]
+   
+   
+                duration-300"
+                prefetch={false}
+              >
+                সুবিধা
+              </Link>
+            </NavigationMenuLink>
+            <NavigationMenuLink asChild>
+              <Link
+                href="#feature"
+                className="group inline-flex h-9 w-max items-center justify-center   
+             px-4 py-2 text-lg 2xl:text-2xl font-medium transition-all 
+             text-[#212833] hover:shadow-[0_4px_2px_-2px_rgba(229,73,129,0.9)]
+
+
+             duration-300"
+                prefetch={false}
+              >
+                বৈশিষ্ট্য
+              </Link>
+            </NavigationMenuLink>
+            <NavigationMenuLink asChild>
+              <Link
+                href="#works"
+                className="group inline-flex h-9 w-max items-center justify-center   
+             px-4 py-2 text-lg 2xl:text-2xl font-medium transition-all 
+             text-[#212833] hover:shadow-[0_4px_2px_-2px_rgba(229,73,129,0.9)]
+
+
+             duration-300"
+                prefetch={false}
+              >
+                ব্লগ
+              </Link>
+            </NavigationMenuLink>
+            <NavigationMenuLink asChild>
+              <Link
+                href="#works"
+                className="group inline-flex h-9 w-max items-center justify-center   
+             px-4 py-2 text-lg 2xl:text-2xl font-medium transition-all 
+             text-[#212833] hover:shadow-[0_4px_2px_-2px_rgba(229,73,129,0.9)]
+
+
+             duration-300"
+                prefetch={false}
+              >
+                যোগাযোগ
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <div>
+        <Link
+          href="/signin"
+          className="bg-purple-700 uppercase text-lg px-7 py-2 text-white rounded-full font-medium 2xl:px-8 2xl:py-4"
+        >
+          লগইন
+        </Link>
+      </div>
+    </header>
   );
 }
