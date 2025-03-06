140ce3d2ff7e0bdf10e7605c8a3176e633ef0710
farhanpavel <farhanpavel3@gmail.com>
2025-03-07 00:23:08 +0600

    clubdashboard renamed

--- client/src/app/(club)/clubdashboard/event/new/page.js
+++ client/src/app/(club)/clubdashboard/event/new/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { Calendar1Icon } from "lucide-react";
+import { Calendar1Icon, ListCollapse } from "lucide-react";
 import { Loader2, UserPlus } from "lucide-react";
 import React, { useState } from "react";
 import {
@@ -83,11 +83,11 @@ export default function Page() {
     <div>
       <div className="p-9 space-y-2">
         <div className="flex gap-x-2 items-center text-black">
-          <UserPlus className="text-3xl" />
-          <h1 className="text-2xl font-bold font-bangla">প্রবেশ</h1>
+          <ListCollapse className="text-3xl" />
+          <h1 className="text-2xl font-bold font-bangla">বিবরণ</h1>
         </div>
         <p className="text-xs text-[#4a4a4a] border-black border-b-[2px] pb-4">
-          আপনার শিক্ষার জন্য একটি নিখুঁত রোডম্যাপ তৈরি করুন।
+          আপনার ইভেন্টের ডাটা গুলো বিস্তারিতভাবে প্রদান করুন।
         </p>
         <div>
           <Card className="border-[1px] border-gray-300">
