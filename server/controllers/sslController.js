8fc0268d474b26b32b74b6c109464715eb679991
farhanpavel <farhanpavel3@gmail.com>
2025-03-23 17:29:10 +0600

    fix

--- server/controllers/sslController.js
+++ server/controllers/sslController.js
@@ -16,7 +16,7 @@ export const initiatePayment = async (req, res) => {
     total_amount: amount,
     currency: "BDT",
     tran_id: uuidv4(),
-    success_url: `http://192.168.0.106:4000/api/ssl/success?userId=${userId}&menuId=${menuId}&quantity=${quantity}&paid=${paid}`,
+    success_url: `https://bidyarthi.onrender.com/api/ssl/success?userId=${userId}&menuId=${menuId}&quantity=${quantity}&paid=${paid}`,
     fail_url: "https://bidyarthi.vercel.app/userdashboard/meal",
     cancel_url: "http://localhost:3030/cancel",
     ipn_url: "http://localhost:3030/ipn",
